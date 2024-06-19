4c7bce814b7c5fe9ad0f9f3d330603bba153afcc
Gustavo Trevizani <67985470+GustavoTrevizani@users.noreply.github.com>
2024-06-19 20:44:14 -0300

    Update PROJETO 1 PARTE 1 SEMANA 1.py

--- PROJETO 1 PARTE 1 SEMANA 1 (1).py
+++ PROJETO 1 PARTE 1 SEMANA 1 (1).py
@@ -1,4 +1,4 @@
 # Faça um programa que mostre 3 mensagens na tela, uma em cada linha: a) seu nome; b) seu endereço; c) sua idade.
 print ("Olá, me chamo Gustavo Trevizani!")
-print ("Meu endereço é Rua Mostardeiros, n° 10, Riozinho/RS.")
+print ("Moro em Riozinho/RS.")
 print ("Eu tenho 15 anos de idade.")
